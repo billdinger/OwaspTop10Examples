8797e985117ea6010b92294dc5632a31cb04ec8b
Bill Dinger <bill.dinger@vml.com>
2019-03-10 17:43:43 -0500

    Fixing some broken functionality!

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+
+}
